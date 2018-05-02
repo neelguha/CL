916636ee7a832cc99cdd9b3699698be821eb119f
Neel Guha <neelguha@Neels-MacBook-Pro-2.local>
2018-05-01 22:40:35 -0700

    adding trials

--- tensorflow/vary_data_mixing_experiment.py
+++ tensorflow/vary_data_mixing_experiment.py
@@ -58,118 +58,119 @@ def main():
     gamma_vals = np.arange(1.0, 0.45, 0-.025)
 
     for gamma in tqdm(gamma_vals):
-        results = {}
-        # Build a new model each time
-        mt.mix_data(gamma)
-        digits1, digits2, gold = new_models(mt)
+        for trial in range(5):
+            results = {}
+            # Build a new model each time
+            mt.mix_data(gamma)
+            digits1, digits2, gold = new_models(mt)
 
-        c = CombinedModel()
+            c = CombinedModel()
 
-        table = []
-        table.append(["Agent Hidden Nodes", FLAGS.hidden])
-        table.append(["Epsilon", FLAGS.epsilon])
-        table.append(["Controller Dataset size", FLAGS.v_size])
-        table.append(["Delta", FLAGS.delta])
-        table.append(["Agent Sample", FLAGS.agent_sample])
-        table.append(["Data Mixing Factor", gamma])
-        table.append(["Weight Initialization", FLAGS.set_weights])
+            table = []
+            table.append(["Agent Hidden Nodes", FLAGS.hidden])
+            table.append(["Epsilon", FLAGS.epsilon])
+            table.append(["Controller Dataset size", FLAGS.v_size])
+            table.append(["Delta", FLAGS.delta])
+            table.append(["Agent Sample", FLAGS.agent_sample])
+            table.append(["Data Mixing Factor", gamma])
+            table.append(["Weight Initialization", FLAGS.set_weights])
 
-        # Store experiment parameters
-        results["agent hidden nodes"] = FLAGS.hidden
-        results["epsilon"] = FLAGS.epsilon
-        results["Controller Dataset size"] = FLAGS.v_size
-        results["Delta"] =  FLAGS.delta
-        results["Agent Sample"] =  FLAGS.agent_sample
-        results["Data Mixing Factor"] = gamma
-        results["Weight Initialization"] = FLAGS.set_weights
-        #print(tabulate(table, headers=["Parameter", "Value"], tablefmt="fancy_grid"))
+            # Store experiment parameters
+            results["agent hidden nodes"] = FLAGS.hidden
+            results["epsilon"] = FLAGS.epsilon
+            results["Controller Dataset size"] = FLAGS.v_size
+            results["Delta"] =  FLAGS.delta
+            results["Agent Sample"] =  FLAGS.agent_sample
+            results["Data Mixing Factor"] = gamma
+            results["Weight Initialization"] = FLAGS.set_weights
+            #print(tabulate(table, headers=["Parameter", "Value"], tablefmt="fancy_grid"))
 
-        d1_sample = mt.mnist1_train.next_batch(FLAGS.agent_sample)
-        d2_sample = mt.mnist2_train.next_batch(FLAGS.agent_sample)
-        m1_spaces = []
-        m2_spaces = []
+            d1_sample = mt.mnist1_train.next_batch(FLAGS.agent_sample)
+            d2_sample = mt.mnist2_train.next_batch(FLAGS.agent_sample)
+            m1_spaces = []
+            m2_spaces = []
 
-        for h in range(FLAGS.hidden):
-            m1_spaces.append(digits1.get_hidden_neuron_space(h, layer=0, data=d1_sample, epsilon=FLAGS.epsilon,
-                                                             delta=FLAGS.delta))
-            m2_spaces.append(digits2.get_hidden_neuron_space(h, layer=0, data=d2_sample, epsilon=FLAGS.epsilon,
-                                                             delta=FLAGS.delta))
+            for h in range(FLAGS.hidden):
+                m1_spaces.append(digits1.get_hidden_neuron_space(h, layer=0, data=d1_sample, epsilon=FLAGS.epsilon,
+                                                                 delta=FLAGS.delta))
+                m2_spaces.append(digits2.get_hidden_neuron_space(h, layer=0, data=d2_sample, epsilon=FLAGS.epsilon,
+                                                                 delta=FLAGS.delta))
 
-        costs = np.ones((FLAGS.hidden, FLAGS.hidden))
-        for d1_n in range(FLAGS.hidden):
-            for d2_n in range(FLAGS.hidden):
+            costs = np.ones((FLAGS.hidden, FLAGS.hidden))
+            for d1_n in range(FLAGS.hidden):
+                for d2_n in range(FLAGS.hidden):
+                    dist = model_utils.get_full_neuron_distance(d1_n, d2_n, digits1, digits2)
+                    if dist < m1_spaces[d1_n] + m2_spaces[d2_n]:
+                        costs[d1_n, d2_n] = 0.0
+
+            d1_neurons, d2_neurons = linear_sum_assignment(costs)
+            for d1_n, d2_n in zip(d1_neurons, d2_neurons):
                 dist = model_utils.get_full_neuron_distance(d1_n, d2_n, digits1, digits2)
                 if dist < m1_spaces[d1_n] + m2_spaces[d2_n]:
-                    costs[d1_n, d2_n] = 0.0
+                    new_w_in, new_b, new_w_out = model_utils.average_hidden_neuron_radius_wise(digits1, digits2, d1_n, d2_n,
+                                                                                               m1_spaces[d1_n], m2_spaces[d2_n])
+                    c.add_hidden_neuron(new_w_in, new_w_out, new_b)
+                else:
+                    d1w1, d1b1 = digits1.get_parameters(0)
+                    d1w2, d1b2 = digits1.get_parameters(1)
 
-        d1_neurons, d2_neurons = linear_sum_assignment(costs)
-        for d1_n, d2_n in zip(d1_neurons, d2_neurons):
-            dist = model_utils.get_full_neuron_distance(d1_n, d2_n, digits1, digits2)
-            if dist < m1_spaces[d1_n] + m2_spaces[d2_n]:
-                new_w_in, new_b, new_w_out = model_utils.average_hidden_neuron_radius_wise(digits1, digits2, d1_n, d2_n,
-                                                                                           m1_spaces[d1_n], m2_spaces[d2_n])
-                c.add_hidden_neuron(new_w_in, new_w_out, new_b)
-            else:
-                d1w1, d1b1 = digits1.get_parameters(0)
-                d1w2, d1b2 = digits1.get_parameters(1)
+                    d2w1, d2b1 = digits2.get_parameters(0)
+                    d2w2, d2b2 = digits2.get_parameters(1)
 
-                d2w1, d2b1 = digits2.get_parameters(0)
-                d2w2, d2b2 = digits2.get_parameters(1)
+                    c.add_hidden_neuron(d1w1[:, d1_n], d1w2[d1_n, :], d1b1[d1_n])
+                    c.add_hidden_neuron(d2w1[:, d2_n], d2w2[d2_n, :], d2b1[d2_n])
 
-                c.add_hidden_neuron(d1w1[:, d1_n], d1w2[d1_n, :], d1b1[d1_n])
-                c.add_hidden_neuron(d2w1[:, d2_n], d2w2[d2_n, :], d2b1[d2_n])
+            for o_n in range(10):
+                _, d1b2 = digits1.get_parameters(1)
+                _, d2b2 = digits2.get_parameters(1)
+                c.add_output_bias(o_n, 0.5 * d1b2[o_n] + 0.5 * d2b2[o_n])
 
-        for o_n in range(10):
-            _, d1b2 = digits1.get_parameters(1)
-            _, d2b2 = digits2.get_parameters(1)
-            c.add_output_bias(o_n, 0.5 * d1b2[o_n] + 0.5 * d2b2[o_n])
-
-        table = []
-        accuracy, loss = digits1.evaluate(mt.mnist1_test)
-        table.append(["Digits1 on Mnist1", accuracy, loss])
-        results["Digits1 on Mnist1"] = (accuracy, loss)
-        accuracy, loss = digits2.evaluate(mt.mnist2_test)
-        table.append(["Digits2 on Mnist2", accuracy, loss])
-        results["Digits2 on Mnist2"] = (accuracy, loss)
-        #print(tabulate(table, headers=["Local Agent Performance", "Accuracy", "Loss"], tablefmt="fancy_grid"))
+            table = []
+            accuracy, loss = digits1.evaluate(mt.mnist1_test)
+            table.append(["Digits1 on Mnist1", accuracy, loss])
+            results["Digits1 on Mnist1"] = (accuracy, loss)
+            accuracy, loss = digits2.evaluate(mt.mnist2_test)
+            table.append(["Digits2 on Mnist2", accuracy, loss])
+            results["Digits2 on Mnist2"] = (accuracy, loss)
+            #print(tabulate(table, headers=["Local Agent Performance", "Accuracy", "Loss"], tablefmt="fancy_grid"))
 
 
-        # BASELINE TESTS
-        table = []
-        accuracy, loss = gold.evaluate(mt.all_test)
-        table.append(["Gold", accuracy, loss])
-        results["Gold"] = (accuracy, loss)
-        accuracy, loss = digits1.evaluate(mt.all_test)
-        table.append(["Digits1", accuracy, loss])
-        results["Digits1"] = (accuracy, loss)
-        accuracy, loss = digits2.evaluate(mt.all_test)
-        table.append(["Digits2", accuracy, loss])
-        results["Digits2"] = (accuracy, loss)
-        # Average models
-        avg_m = model_utils.average_models(digits1, digits2)
-        accuracy, loss = avg_m.evaluate(mt.all_test)
-        table.append(["Averaged", accuracy, loss])
-        results["Averaged"] = (accuracy, loss)
-        #print(tabulate(table, headers=["Baseline Performance", "Accuracy", "Loss"], tablefmt="fancy_grid"))
+            # BASELINE TESTS
+            table = []
+            accuracy, loss = gold.evaluate(mt.all_test)
+            table.append(["Gold", accuracy, loss])
+            results["Gold"] = (accuracy, loss)
+            accuracy, loss = digits1.evaluate(mt.all_test)
+            table.append(["Digits1", accuracy, loss])
+            results["Digits1"] = (accuracy, loss)
+            accuracy, loss = digits2.evaluate(mt.all_test)
+            table.append(["Digits2", accuracy, loss])
+            results["Digits2"] = (accuracy, loss)
+            # Average models
+            avg_m = model_utils.average_models(digits1, digits2)
+            accuracy, loss = avg_m.evaluate(mt.all_test)
+            table.append(["Averaged", accuracy, loss])
+            results["Averaged"] = (accuracy, loss)
+            #print(tabulate(table, headers=["Baseline Performance", "Accuracy", "Loss"], tablefmt="fancy_grid"))
 
-        #print("%d total neurons in aggregate model" % c.hidden)
-        results["Aggregated Total Neurons"] = c.hidden
+            #print("%d total neurons in aggregate model" % c.hidden)
+            results["Aggregated Total Neurons"] = c.hidden
 
-        table = []
-        new_model = c.convert_to_model()
-        accuracy, loss = new_model.evaluate(mt.all_test)
-        table.append(["Aggregate", accuracy, loss])
-        results["Aggregate"] = (accuracy, loss)
+            table = []
+            new_model = c.convert_to_model()
+            accuracy, loss = new_model.evaluate(mt.all_test)
+            table.append(["Aggregate", accuracy, loss])
+            results["Aggregate"] = (accuracy, loss)
 
-        full_d_sample = mt.mnist1_train.next_batch(FLAGS.v_size / 2)
-        full_d_sample.add_data_obj(mt.mnist2_train.next_batch(FLAGS.v_size / 2))
-        new_model.train_second_layer(full_d_sample, iters=2000)
-        accuracy, loss = new_model.evaluate(mt.all_test)
-        table.append(["Tuned Aggregate", accuracy, loss])
-        results["Tuned Aggregate"] = (accuracy, loss)
+            full_d_sample = mt.mnist1_train.next_batch(FLAGS.v_size / 2)
+            full_d_sample.add_data_obj(mt.mnist2_train.next_batch(FLAGS.v_size / 2))
+            new_model.train_second_layer(full_d_sample, iters=2000)
+            accuracy, loss = new_model.evaluate(mt.all_test)
+            table.append(["Tuned Aggregate", accuracy, loss])
+            results["Tuned Aggregate"] = (accuracy, loss)
 
-        #print(tabulate(table, headers=["Exp", "Accuracy", "Loss"], tablefmt="fancy_grid"))
-        np.save("experimental_results/data_mixing/%f" % gamma, results)
+            #print(tabulate(table, headers=["Exp", "Accuracy", "Loss"], tablefmt="fancy_grid"))
+            np.save("experimental_results/data_mixing/%f_%d" % (gamma, trial), results)
 
 
 
